f123867d4cc05e96905fbdc0dc5f6758dbf0d893
enesaksuu34 <88115318+enesaksuu34@users.noreply.github.com>
2023-06-22 23:25:14 +0000

    sitemap.xml edited.

--- generateSitemap.mjs
+++ generateSitemap.mjs
@@ -6,22 +6,21 @@ function generateSitemap() {
   const posts = getPosts();
   const baseUrl = 'https://blog.hyaena.co.uk'; // Replace this with your base website URL
 
-  const sitemapXML = `
-  <?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${posts
-        .map((post) => {
-          const slug = post.filePath.replace(/\.mdx?$/, '');
-          const url = `${baseUrl}/posts/${slug}`;
-          return `
-          <url>
-              <loc>${url}</loc>
-          </url>
-          `;
-        })
-        .join('\n')}
-  </urlset>
-  `;
+  const sitemapXML = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${posts
+      .map((post) => {
+        const slug = post.filePath.replace(/\.mdx?$/, '');
+        const url = `${baseUrl}/posts/${slug}`;
+        return `
+        <url>
+            <loc>${url}</loc>
+        </url>
+        `;
+      })
+      .join('\n')}
+</urlset>
+`;
 
   const publicDir = path.join(process.cwd(), 'public');
   if (!fs.existsSync(publicDir)) {
