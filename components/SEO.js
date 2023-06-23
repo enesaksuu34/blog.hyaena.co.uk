1f3aa46674f0fceaad2f7d47fbc1458e5d54a928
enesaksuu34 <88115318+enesaksuu34@users.noreply.github.com>
2023-06-23 05:19:31 +0300

    added yandex-verification

--- components/SEO.js
+++ components/SEO.js
@@ -7,6 +7,7 @@ export default function SEO({ title, description }) {
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta name="google-site-verification" content="BE1E1WedLySO32waPep7Iw7VgIe7kYZiZJRQoX661G0" />
+      <meta name="yandex-verification" content="370531bcef5bc805" />
     </Head>
   );
 }
