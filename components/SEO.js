ad51b9826c9c54bb8de4989f81064c0e88854d5d
enesaksuu34 <88115318+enesaksuu34@users.noreply.github.com>
2023-06-23 05:27:49 +0300

    added google analytics

--- components/SEO.js
+++ components/SEO.js
@@ -8,6 +8,15 @@ export default function SEO({ title, description }) {
       <meta property="og:title" content={title} />
       <meta name="google-site-verification" content="BE1E1WedLySO32waPep7Iw7VgIe7kYZiZJRQoX661G0" />
       <meta name="yandex-verification" content="370531bcef5bc805" />
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-CLL1CRSFF1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-CLL1CRSFF1');
+      </script>
     </Head>
   );
 }
